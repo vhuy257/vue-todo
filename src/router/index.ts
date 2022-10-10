9a98767caa5813fec3b0c19e974a4b98e5779aee
unknown <vhuy2571990@gmail.com>
2022-10-10 17:09:01 +0700

    Fix issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,35 +15,35 @@ const router = createRouter({
             name: 'category',
             meta: { title: 'Category' },
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/CategoryView.vue')
+            component: () => import('@/views/CategoryView.vue')
         },
         {
             path: '/product/:id',
             name: 'product-detail',
             meta: { title: 'Product detail' },
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/ProductView.vue')
+            component: () => import('@/views/ProductView.vue')
         },
         {
             path: '/blog',
             name: 'blog',
             meta: { title: 'Blog' },
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/BlogView.vue')
+            component: () => import('@/views/BlogView.vue')
         },
         {
             path: '/cart',
             name: 'cart',
             meta: { title: 'Cart' },
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/CartView.vue')
+            component: () => import('@/views/CartView.vue')
         },
         {
             path: '/checkout',
             name: 'checkout',
             meta: { title: 'Checkout' },
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/CheckoutView.vue')
+            component: () => import('@/views/CheckoutView.vue')
         }
     ]
 });
